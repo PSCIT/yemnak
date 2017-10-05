a1c3bde826217a3617ddf909cdc06164be6c051c
Kamal Ibrahim <kamal_i88@outlook.com>
2017-10-05 14:15:17 +0300

    add model and Membership controller and classic view .
    add model and Membership controller and classic view .

---
+++ yemnak/Controllers/MemberShipController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace yemnak.Controllers
+{
+    public class MemberShipController : Controller
+    {
+        // GET: MemberShip
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Classic()
+        {
+
+            return View();
+        }
+
+
+
+    }
+}
