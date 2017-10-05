724006e1365c8543a4ab363e260c2ddfa4744811
Shabbir Ismail <shabbir7252@live.com>
2017-10-05 15:10:25 +0300

    CultureInfo setting to other Controller
    Added cultural info to Membership Controller

--- yemnak/Controllers/MemberShipController.cs
+++ yemnak/Controllers/MemberShipController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,23 @@ namespace yemnak.Controllers
 {
     public class MemberShipController : Controller
     {
-        // GET: MemberShip
-        public ActionResult Index()
+        private void languageSwitch(string language)
         {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(language);
+            if (language == "ar-KW")
+            {
+                ViewBag.language = "ar-KW";
+            }
+            else
+            {
+                ViewBag.language = "en-US";
+            }
+        }
+
+        public ActionResult Index(string language)
+        {
+            languageSwitch(language);
             return View();
         }
 
